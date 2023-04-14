c5ab315707362d19f3f9f6826b6b771950051651
thomas <thomas.feuillatre.pro@gmail.com>
2023-04-14 17:04:48 +0200

    update

--- appli-rapha/src/views/Troiscausedelechecdelaprohibition.vue
+++ appli-rapha/src/views/Troiscausedelechecdelaprohibition.vue
@@ -1,1 +1,27 @@
-<template></template>
+<script></script>
+
+<template>
+   <p>Au moment où la prohibition a été adoptée, l'alcool était pratiquement le seul psychotrope consommé aux États-Unis et il l'était dans des proportions bien</p>
+   <p>plus importantes qu'aujourd'hui. Mais le <a href="https://fr.wikipedia.org/wiki/Pouvoir_l%C3%A9gislatif" target="_blank">législateur</a> ne savait pas à l'époque avec certitude que jamais aucune mesure coercitive, dans aucun</p>
+   <p>pays, n'était parvenue à mettre un terme à la sur-consommation de substances <a href="https://fr.wikipedia.org/wiki/Psychotrope" target="_blank">psychotropes</a> quelles qu'elles soient. Il ne mesurait pas vraiment l'ampleur</p>
+   <p>insurmontable de la tâche. Selon l' <a href="https://fr.wikipedia.org/wiki/Organisation_mondiale_de_la_sant%C3%A9" target="_blank">OMS</a> et l' <a href="https://fr.wikipedia.org/wiki/Association_am%C3%A9ricaine_de_psychiatrie" target="_blank">association mondiale des psychiatres</a>, l'alcoolisme est une maladie mentale majeure (une <a href="https://fr.wikipedia.org/wiki/Psychose" target="_blank">"psychose"</a>) et son</p>
+   <p>traitement repose sur la volonté intrinsèque de l'individu à en guérir pour limiter au maximum les risques de rechute. En somme, une loi ne semble pas</p>
+   <p>être suffisante pour traiter un sujet sanitaire aussi complexe.</p>
+   <p>L'absurdité, l'inefficacité et les multiples conséquences néfastes d'une telle <a href="https://fr.wikipedia.org/wiki/Loi" target="_blank">loi</a> entraîne inévitablement un jour, son abrogation. Concernant plus</p>
+   <p>précisément celle qui a été adoptée aux États-Unis, il est possible d'identifier des facteurs ampliatifs qui ont conduit à son fiasco spectaculaire et à son</p>
+   <p>abandon rapide : </p>
+   <p>-la première raison est la difficile tâche de coordonner les autorités de chaque État (police, gouvernement…) avec celles du gouvernement fédéral. Ce</p>
+   <p>manque évident de coordination avait rendu plus ardue l'application de la loi ;</p>
+   <p>-la deuxième raison est l'argent perdu par l'État au profit de gangsters tels qu'Al Capone durant les années de la prohibition, car ce trafic se montait à</p>
+   <p>plusieurs dizaines de millions de dollars. Des enquêtes furent menées et des révisions suggérées pour le dix-huitième amendement mais rien ne</p>
+   <p>bougea avant 1933, l'année de la mise en place du vingt-et-unième amendement ;</p>
+   <p>la troisième et principale raison fut la <a href="https://fr.wikipedia.org/wiki/Grande_D%C3%A9pression" target="_blank">crise économique qui frappa les États-Unis en 1929</a>. Cette crise força le gouvernement fédéral à imposer des</p>
+   <p>taxes là où il le pouvait, c'est-à-dire sur le commerce de l'alcool, qui constituait une grande source de revenu pour l'État. De plus, après l'entrée en</p>
+   <p>vigueur du dix-huitième amendement, le taux de chômage étant en hausse constante, l'État ainsi que la population avaient perdu des sommes</p>
+   <p>considérables lors de la fermeture de distilleries.</p>
+   <p>Lorsque cessa la prohibition, les organisations criminelles perdirent une part importante de leurs revenus liés au marché noir de l'alcool, conséquence</p>
+   <p>directe de la concurrence des boissons alcoolisées en vente libre et à prix modéré. Elles se recyclèrent alors dans la vente d'autres drogues illicites, les</p>
+   <p> <a href="https://fr.wikipedia.org/wiki/Jeu_de_hasard" target="_blank">jeux de hasard</a>, le <a href="https://fr.wikipedia.org/wiki/Racket" target="_blank">racket</a> de protection et la <a href="https://fr.wikipedia.org/wiki/Prostitution" target="_blank">prostitution</a>.</p>
+   <p>Après le retrait de la loi fédérale, le <a href="https://fr.wikipedia.org/wiki/Kansas" target="_blank">Kansas</a> jusqu'en 1948 et le <a href="https://fr.wikipedia.org/wiki/Mississippi_(%C3%89tat)" target="_blank">Mississippi</a> jusqu'en 1966 ont continué à imposer la prohibition. De son côté, l'<a href="https://fr.wikipedia.org/wiki/Oklahoma" target="_blank">Oklahoma</a> a</p>
+   <p>continué à imposer une prohibition partielle, qui ne concernait que les liqueurs (i.e. tout alcool titrant à plus de 3,2 <a href="https://fr.wikipedia.org/wiki/Degr%C3%A9_d%27alcool" target="_blank">degrés</a>), jusqu'en 1959.</p>
+</template>
